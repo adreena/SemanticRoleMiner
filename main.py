a9b22393c61ce947f119aeacb710983b1152398c
Kimia Hszd <kim.hszd@gmail.com>
2012-11-19 17:05:15 -0700

    cleaning my code

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 #importing SenSta class for creating 
 from StanSennaClass import SenSta
-from FindPropArg import findArg
-from FindPropArg import findPreds
-from FindPropArg import mixDepArg
-from FindPropArg import findDomain
+from FindPropArg import Find_Pred_Arg_Root
+from FindPropArg import Find_ArgDom_MixArgDep
 from CreateRelations import makeStatements
-from CreateRelations import subSt
-from CreateRelations import verbDepArg
 from Visualizer import makeGephi
 
 
@@ -15,43 +11,23 @@ inputFile="srl/python/SemanticRoleMiner/testCases/test1"
 myTestFile=SenSta(inputFile)
 myTestFile.makeSenna()
 myTestFile.makeStanf()
-
-
 SEN0_SE=myTestFile.sennaDict['sen0']
 SEN0_ST=myTestFile.stanfDict['sen0']
-root=myTestFile.findRoot(SEN0_ST)
-allPreds=findPreds(SEN0_SE)
 
-
-#---- testing predicates ------------
-i=1 # 0 is reserved for root
-PRED={}
-ARGS={}
-for item in allPreds.values():
-    verb=item[0][0]
-    col=item[1]
-    if verb==root: #---if the verb is root then it takes 0 index 
-       PRED[0]=verb
-       ARGS[0]=findArg(root,SEN0_SE,col)
-    else:
-       PRED[i]=verb
-       ARGS[i]=findArg(verb,SEN0_SE,col)
-       i+=1 
-
+#------------------------------------
+# Find Predicates,root, and all Args for each predicate
+PAR=Find_Pred_Arg_Root(myTestFile,SEN0_SE,SEN0_ST)
+PRED=PAR[0]
+ARGS=PAR[1]
+root=PAR[2]
 
 #-------------------------------------
-# Domain of each arg
-ARGDOM={} #
-MIXARGS={}
-for key,val in ARGS.items():
-    ARGDOM[key]=findDomain(val)
-    MIXARGS[key]=mixDepArg(SEN0_ST,ARGS[key],PRED[key])
+# Find domain of each predicate and group all dependencies under each Args
 
+AM=Find_ArgDom_MixArgDep(ARGS,PRED,SEN0_ST)
+ARGDOM=AM[0]
+MIXARGS=AM[1]
 
-#-----------------------------------
-
-statement={}
-start=100
 
 #-----------------------------------
 # manipulating all statements 
