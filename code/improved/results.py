87357d720e9d4ace318b1f96e66c3a9c3a99f2b0
Kimia Hszd <kim.hszd@gmail.com>
2013-02-14 16:07:00 -0700

    fixed 's which is a bug in senna n form of -of- relation

---
+++ code/improved/results.py
@@ -0,0 +1,43 @@
+-Complete Sentence is: 
+the Missouri ,maker of Diamond, PremiumEdge, KirklandSignature, and other pet food brands has not called special attention to the expansion of the recall for cat food beyond amending a statement on the company Internet recall site .
+
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+0-the Missouri , maker of Diamond , PremiumEdge , KirklandSignature other pet food brands not called special attention to the expansion of the recall cat food  .
+
+      Missouri-2   is   LOC
+      Diamond-6   is   PER
+      PremiumEdg-8   is   PER
+      KirklandSignatur-10   is   PER
+      KirklandSignature-10  is  caller
+      brand-14  is  caller
+      not-15  is  NEG
+      attend-18  is  item-being-labelled
+      expands-21  is  attribute-of-arg1
+      KirklandSignature-10  subject  Missouri-2
+      Missouri-2  sameAs  maker-4
+      PremiumEdge-8  --  Diamond-6
+      maker-4  of  PremiumEdge-8
+      brand-14  is  other-11
+      brand-14  is  pet-12
+      brand-14  --  food-13
+      call-16  subject  brand-14
+      call-16  not  not-15
+      attend-18  is  special-17
+      call-16  object  attend-18
+      call-16  to  expands-21
+      food-26  --  regal-24
+      food-26  --  cat-25
+      expands-21  of  food-26
+          --------------------------------------------------          
+1-amending a statement on the company Internet recall site  .
+
+      Internet-7   is   MISC
+      statement-3  is  thing-amended
+      Internet-7  is  secondary-predication-on-arg1
+      regal-8  is  secondary-predication-on-arg1
+      amend-1  object  statement-3
+      Internet-7  --  company-6
+      amend-1  on  Internet-7
+      regal-8  object  site-9
+          --------------------------------------------------          
